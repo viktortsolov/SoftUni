444f71e952eac04802f2926d16e09a1243953fbe
Viktor Tsolov <viktortsolov03@gmail.com>
2021-01-15 19:02:58 +0200

    03. Openning

---
+++ 02. CSharp-Advanced/01. CSharp-Advanced/ExamPreparation - CSharpAdvanced/03. Openning/Bakery.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    class Bakery
+    {
+        private List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+
+            data = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count
+            => data.Count();
+
+        public void Add(Employee employee)
+        {
+            if (data.Count() < Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee employee = data.FirstOrDefault(x => x.Name == name);
+            if (data.Contains(employee))
+            {
+                data.Remove(employee);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(x => x.Age).First();
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+
+            foreach (var item in data)
+            {
+                sb.AppendLine($"{item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
