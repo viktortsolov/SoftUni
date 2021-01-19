52e4ef55b697c1c2024cf6b6c248b350e9ab5cf7
Viktor Tsolov <viktortsolov03@gmail.com>
2021-01-19 08:25:10 +0200

    Recursion - 8th Queens

--- 02. CSharp-Advanced/01. CSharp-Advanced/00. Recursion - Bonus/EightQueensProblem/Program.cs
+++ 02. CSharp-Advanced/01. CSharp-Advanced/00. Recursion - Bonus/EightQueensProblem/Program.cs
@@ -10,26 +10,51 @@ namespace EightQueensProblem
 
             int[,] matrix = new int[queens, queens];
 
-            GetQueens(matrix, 0);
+            Console.WriteLine(GetQueens(matrix, 0));
+
         }
 
-        private static void GetQueens(int[,] queens, int row)
+        static void Print(int[,] queens)
+        {
+            for (int row = 0; row < queens.GetLength(0); row++)
+            {
+                for (int col = 0; col < queens.GetLength(1); col++)
+                {
+                    if (queens[row, col] == 1)
+                    {
+                        Console.Write($"Q");
+                    }
+                    if (queens[row, col] == 0)
+                    {
+                        Console.Write($"_");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private static int GetQueens(int[,] queens, int row)
         {
             if (row == queens.GetLength(0))
             {
-                Console.WriteLine("Queens found!");
-                return;
+                Print(queens);
+                return 1;
             }
+
+            int foundQueens = 0;
 
             for (int col = 0; col < queens.GetLength(1); col++)
             {
                 if (IsSafe(queens, row, col))
                 {
                     queens[row, col] = 1;
-                    GetQueens(queens, row + 1);
+                    foundQueens += GetQueens(queens, row + 1);
                     queens[row, col] = 0;
                 }
             }
+
+            return foundQueens;
         }
 
         private static bool IsSafe(int[,] queens, int row, int col)
@@ -44,15 +69,42 @@ namespace EightQueensProblem
                 {
                     return false;
                 }
-                if (row + i >= 0 && queens[row + i, col] == 1)
+                if (row + i < queens.GetLength(0) && queens[row + i, col] == 1)
                 {
                     return false;
                 }
-                if (col + 1 >= 0 && queens[row, col + 1] == 1)
+                if (col + i < queens.GetLength(1) && queens[row, col + i] == 1)
+                {
+                    return false;
+                }
+
+                if (col + i < queens.GetLength(0) &&
+                    row + i < queens.GetLength(0) &&
+                    queens[row + i, col + i] == 1) 
+                {
+                    return false;
+                }
+                if (col - i >= 0 &&
+                    row + i < queens.GetLength(0) &&
+                    queens[row + i, col - i] == 1)
+                {
+                    return false;
+                }
+                if (col - i >= 0 &&
+                    row - i >= 0 &&
+                    queens[row - i, col - i] == 1)
+                {
+                    return false;
+                }
+
+                if (col + i < queens.GetLength(0) &&
+                    row - i >= 0 &&
+                    queens[row - i, col + i] == 1)
                 {
                     return false;
                 }
             }
+            return true;
         }
     }
 }
