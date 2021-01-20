366d519a2aeaf29968683dcecddc4a87cdb36569
Viktor Tsolov <viktortsolov03@gmail.com>
2021-01-20 16:44:24 +0200

    Inheritance - Exercise

---
+++ 02. CSharp-Advanced/02. CSharp-OOP/01. Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format($"Name: {Name}, Age: {Age}"));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ 02. CSharp-Advanced/02. CSharp-OOP/01. Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultCoffeeMilliliters = 50;
+        private const decimal DefaultCoffeePrice = 3.50M;
+
+        public double Caffeine { get; set; }
+
+        public Coffee(string name, double caffeine) 
+            : base(name, DefaultCoffeePrice, DefaultCoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+    }
+}
