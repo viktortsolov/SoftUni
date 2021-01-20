4eab8cd094eabc97e84afde5aebc68bddc8185d4
Viktor Tsolov <viktortsolov03@gmail.com>
2021-01-20 14:46:00 +0200

    Inheritance - Lab

---
+++ 02. CSharp-Advanced/02. CSharp-OOP/01. Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RandomString()
+        {
+            int index = rnd.Next(0, this.Count);
+            string removed = this[index];
+
+            this.RemoveAt(index);
+            return removed;
+        }
+    }
+}
