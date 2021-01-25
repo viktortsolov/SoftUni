716edc2723d631757da3ed47cd280451c2927002
Viktor Tsolov <viktortsolov03@gmail.com>
2021-01-25 16:25:07 +0200

    Polymorphism - Lab

---
+++ 02. CSharp-Advanced/02. CSharp-OOP/04. Polymorphism/Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavouriteFood}";
+        }
+    }
+}
