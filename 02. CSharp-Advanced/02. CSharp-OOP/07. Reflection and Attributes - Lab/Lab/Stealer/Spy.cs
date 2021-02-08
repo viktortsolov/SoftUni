3f64c6c55c77408caa28faa43d06668ff35cb4d9
Viktor Tsolov <viktortsolov03@gmail.com>
2021-02-08 14:58:39 +0200

    Reflection and Attributes - Lab (3&4)

--- 02. CSharp-Advanced/02. CSharp-OOP/07. Reflection and Attributes - Lab/Lab/Stealer/Spy.cs
+++ 02. CSharp-Advanced/02. CSharp-OOP/07. Reflection and Attributes - Lab/Lab/Stealer/Spy.cs
@@ -53,5 +53,45 @@ namespace Stealer
 
             return stringBuilder.ToString().Trim();
         }
+
+        public string RevealPrivateMethods(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"All Private Methods of Class: {investigatedClass}");
+            stringBuilder.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (MethodInfo method in classMethods)
+            {
+                stringBuilder.AppendLine(method.Name);
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+
+        public string CollectGettersAndSetters(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                stringBuilder.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                stringBuilder.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
     }
 }
