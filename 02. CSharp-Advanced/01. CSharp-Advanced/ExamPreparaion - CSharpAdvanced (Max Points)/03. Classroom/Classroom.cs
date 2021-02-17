75c0bfdb9232b37e4d76efd73fae2e3629337c27
Viktor Tsolov <viktortsolov03@gmail.com>
2021-02-17 20:30:19 +0200

    C# Advanced Exam - 25 October 2020 (3rd problem)

---
+++ 02. CSharp-Advanced/01. CSharp-Advanced/ExamPreparaion - CSharpAdvanced (Max Points)/03. Classroom/Classroom.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            this.students = new List<Student>();
+        }
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get
+            {
+                return this.students.Count;
+            }
+        }
+
+        public string RegisterStudent(Student student)
+        {
+            if (this.Count < this.Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = this.students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (students.Contains(student))
+            {
+                students.Remove(student);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            var studentWithSubject = this.students.Where(x => x.Subject == subject);
+
+            if (studentWithSubject.Count() == 0)
+            {
+                return "No students enrolled for the subject";
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Subject: {subject}");
+            stringBuilder.AppendLine("Students:");
+            foreach (var item in studentWithSubject)
+            {
+                stringBuilder.AppendLine($"{item.FirstName} {item.LastName}");
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = this.students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            return student;
+        }
+
+    }
+}
---
+++ 02. CSharp-Advanced/01. CSharp-Advanced/ExamPreparaion - CSharpAdvanced (Max Points)/03. Classroom/Student.cs
@@ -0,0 +1,54 @@
+﻿namespace ClassroomProject
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string subject;
+        public Student(string firstName, string lastName, string subject)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.subject = subject;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                value = firstName;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                value = lastName;
+            }
+        }
+        public string Subject
+        {
+            get
+            {
+                return subject;
+            }
+            set
+            {
+                value = subject;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Student: First Name = {this.firstName}, Last Name = {this.lastName}, Subject = {this.subject}";
+        }
+    }
+}
