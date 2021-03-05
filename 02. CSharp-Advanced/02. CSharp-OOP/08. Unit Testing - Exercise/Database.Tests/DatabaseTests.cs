1b736acff53c0cde976fcd9397377aa1d171daf0
Viktor Tsolov <viktortsolov03@gmail.com>
2021-03-05 16:09:35 +0200

    Unit Testing - Exercise

--- 02. CSharp-Advanced/02. CSharp-OOP/08. Unit Testing - Exercise/Database.Tests/DatabaseTests.cs
+++ 02. CSharp-Advanced/02. CSharp-OOP/08. Unit Testing - Exercise/Database.Tests/DatabaseTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Linq;
 
 namespace Tests
 {
@@ -10,9 +11,38 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void ConstructorShouldBeInitializedWith16Elements()
         {
-            Assert.Pass();
+            int[] numbers = Enumerable.Range(1, 16).ToArray();
+            Database.Database database = new Database.Database(numbers);
+
+            var expectedResult = 16;
+            var actualResult = database.Count;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ConstructorShoulThrowExpectionIfThereAreNot16Elements()
+        {
+            //TODO: Refactor this
+            int[] numbers = Enumerable.Range(1, 10).ToArray();
+            Database.Database database = new Database.Database(numbers);
+
+            var expectedResult = 10;
+            var actualResult = database.Count;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void AddOperationShouldAddElementAtNextFreeCell()
+        {
+            int[] numbers = Enumerable.Range(1, 10).ToArray();
+            Database.Database database = new Database.Database(numbers);
+
+            database.Add(5);
+
         }
     }
 }
