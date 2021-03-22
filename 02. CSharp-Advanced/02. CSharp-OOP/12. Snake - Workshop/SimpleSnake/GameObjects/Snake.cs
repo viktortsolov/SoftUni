3fa9f7ace43ab07d6068aa2dcaf48f231eba112f
Viktor Tsolov <viktortsolov03@gmail.com>
2021-03-22 10:02:47 +0200

    SimpleSnake - TODO: Finish the snake class

---
+++ 02. CSharp-Advanced/02. CSharp-OOP/12. Snake - Workshop/SimpleSnake/GameObjects/Snake.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleSnake.GameObjects
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Snake : Point
+    {
+        private const char snakeSymbol = '\u25CF';
+        private readonly Queue<Point> snakeElements;
+        public Snake(int leftX, int topY) 
+            : base(leftX, topY)
+        {
+            this.snakeElements = new Queue<Point>();
+            this.CreateSnake();
+        }
+
+        public bool isMoving(Point direction)
+        {
+            var head = this.snakeElements.Last();
+
+            return true;
+        }
+
+        private void CreateSnake()
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                var point = new Point(this.LeftX + i, this.TopY);
+                point.Draw(snakeSymbol);
+                snakeElements.Enqueue(point);
+            }
+        }
+    }
+}
--- 02. CSharp-Advanced/02. CSharp-OOP/12. Snake - Workshop/SimpleSnake/StartUp.cs
+++ 02. CSharp-Advanced/02. CSharp-OOP/12. Snake - Workshop/SimpleSnake/StartUp.cs
@@ -10,19 +10,10 @@
             ConsoleWindow.CustomizeConsole();
             Wall wall = new Wall(60, 20);
 
-            //TODO: Can I pass wall.left and wall.top?
             Food food = new FoodDollar(wall);
+            Snake snake = new Snake(1, 1);
 
             food.SetRandomPostition(new System.Collections.Generic.Queue<Point>());
-
-            //TODO: CHeck it up
-
-            /*Point point = new Point(10, 10);
-            point.Draw('*');
-            point.Draw(1, 1, '*');
-            point.Draw(2, 2, '*');
-            point.Draw(0, 1, '*');*/
-
         }
     }
 }
