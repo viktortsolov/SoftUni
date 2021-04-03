1f60e43d62898dea36a8893d6989894df2e17fb0
Viktor Tsolov <viktortsolov03@gmail.com>
2021-04-03 14:09:55 +0300

    Unit Testing - skeleton

---
+++ 02. CSharp-Advanced/02. CSharp-OOP/Exam Preparation/Exam - 16.08.2020/Computers-Skeleton/Computers.Tests/ComputerManagerTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Computers.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddComputerMethodShouldThrowExceptionWhenNullIsPassed()
+        {
+        }
+    }
+}
