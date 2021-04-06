5d73dedd9138c8072323c03aec188ed8a5b6fdbe
Viktor Tsolov <viktortsolov03@gmail.com>
2021-04-06 09:51:11 +0300

    Unit Testing - 50/100

--- 02. CSharp-Advanced/02. CSharp-OOP/Exam Preparation/Exam - 16.08.2020/Computers-Skeleton/Computers.Tests/ComputerManagerTests.cs
+++ 02. CSharp-Advanced/02. CSharp-OOP/Exam Preparation/Exam - 16.08.2020/Computers-Skeleton/Computers.Tests/ComputerManagerTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Computers.Tests
 {
@@ -10,8 +12,95 @@ namespace Computers.Tests
         }
 
         [Test]
-        public void AddComputerMethodShouldThrowExceptionWhenNullIsPassed()
+        public void AddComputerMethodShouldThrowExceptionWhenComputerExists()
         {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Computer computer = new Computer("IBM", "2", 2m);
+                ComputerManager computerManager = new ComputerManager();
+
+                computerManager.AddComputer(computer);
+                computerManager.AddComputer(computer);
+            });
+        }
+            
+        [Test]
+        public void AddComputerMethodShouldThrowExceptionWhenComputerIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                Computer computer = null;
+                ComputerManager computerManager = new ComputerManager();
+
+                computerManager.AddComputer(computer);
+            });
+        }
+
+        [Test]
+        public void GetComputerThrowsExceptionWhenModelIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(()
+                =>
+            {
+                Computer computer = new Computer("IBM", null, 2m);
+                ComputerManager computerManager = new ComputerManager();
+
+                computerManager.AddComputer(computer);
+                computerManager.GetComputer("IBM", null);
+            });
+        }
+
+        [Test]
+        public void GetComputerThrowsExceptionWhenManufacturerIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(()
+                =>
+            {
+                Computer computer = new Computer(null, "yes", 2m);
+                ComputerManager computerManager = new ComputerManager();
+
+                computerManager.AddComputer(computer);
+                computerManager.GetComputer(null, "yes");
+            });
+        }
+
+        [Test]
+        public void GetComputerMethodShouldThrowExceptionWhenComputerIsNull()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                ComputerManager computerManager = new ComputerManager();
+
+                computerManager.GetComputer("sda", "sda");
+            });
+        }
+
+        [Test]
+        public void GetComputerShouldWork()
+        {
+            Computer computer = new Computer("IBM", "A1", 2m);
+            ComputerManager computerManager = new ComputerManager();
+
+            computerManager.AddComputer(computer);
+
+            var expectedResult = computer;
+            var actualResult = computerManager.GetComputer("IBM", "A1");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void RemoveCompuerShouldWork()
+        {
+            Computer computer = new Computer("IBM", "A1", 2m);
+            ComputerManager computerManager = new ComputerManager();
+
+            computerManager.AddComputer(computer);
+
+            var expectedResult = computer;
+            var actualResult = computerManager.RemoveComputer("IBM", "A1");
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
