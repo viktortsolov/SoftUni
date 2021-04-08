56a4dd4e37979754e577e44b2d4a9bb8fa8714c3
Viktor Tsolov <viktortsolov03@gmail.com>
2021-04-08 20:42:57 +0300

    C# OOP Exam - 12.12.2020 - Bus. Logic (Half)

---
+++ 02. CSharp-Advanced/02. CSharp-OOP/Exam Preparation/Exam - 12.12.2020/Bakery/Core/Controller.cs
@@ -0,0 +1,141 @@
+﻿using Bakery.Core.Contracts;
+using Bakery.Models.BakedFoods;
+using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables;
+using Bakery.Models.Tables.Contracts;
+using Bakery.Utilities.Messages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bakery.Core
+{
+    public class Controller : IController
+    {
+        private List<IBakedFood> bakedFoods;
+        private List<IDrink> drinks;
+        private List<ITable> tables;
+
+        public Controller()
+        {
+            bakedFoods = new List<IBakedFood>();
+            drinks = new List<IDrink>();
+            tables = new List<ITable>();
+        }
+
+        public string AddDrink(string type, string name, int portion, string brand)
+        {
+            Drink drink = null;
+            if (type == "Tea")
+            {
+                drink = new Tea(name, portion, brand);
+            }
+            else if (type == "Water")
+            {
+                drink = new Water(name, portion, brand);
+            }
+            else
+            {
+                return null;
+            }
+
+            drinks.Add(drink);
+            return string.Format(OutputMessages.DrinkAdded, name, brand);
+        }
+
+        public string AddFood(string type, string name, decimal price)
+        {
+            BakedFood food = null;
+            if (type == "Bread")
+            {
+                food = new Bread(name, price);
+            }
+            else if (type == "Cake")
+            {
+                food = new Cake(name, price);
+            }
+            else
+            {
+                return null;
+            }
+
+            bakedFoods.Add(food);
+            return string.Format(OutputMessages.FoodAdded, name, type);
+        }
+
+        public string AddTable(string type, int tableNumber, int capacity)
+        {
+            Table table = null;
+            if (type == "InsideTable")
+            {
+                table = new InsideTable(tableNumber, capacity);
+            }
+            else if (type == "OutsideTable")
+            {
+                table = new OutsideTable(tableNumber, capacity);
+            }
+            else
+            {
+                return null;
+            }
+
+            tables.Add(table);
+            return string.Format(OutputMessages.TableAdded, tableNumber);
+        }
+
+        public string GetFreeTablesInfo()
+        {
+            var freeTables = this.tables.Where(x => x.IsReserved == false);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var table in tables)
+            {
+                sb.AppendLine(table.GetFreeTableInfo());
+            }
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
+
+        public string GetTotalIncome()
+        {
+            var result = this.tables.Sum(x => x.GetBill());
+            return string.Format(OutputMessages.TotalIncome, result);
+        }
+
+        public string LeaveTable(int tableNumber)
+        {
+            var table = this.tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+
+            var bill = table.GetBill();
+            table.Clear();
+            //TODO: Do I need to remove it?
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Table: {tableNumber}");
+            sb.AppendLine($"Bill: {bill:f2}");
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+        public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
+        {
+            var table = this.tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+ 
+        }
+
+        public string OrderFood(int tableNumber, string foodName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ReserveTable(int numberOfPeople)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
