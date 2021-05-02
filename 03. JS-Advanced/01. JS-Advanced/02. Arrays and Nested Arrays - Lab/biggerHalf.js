566a1c54c0df070f779aad5a6ff57bc8e927dca7
Viktor Tsolov <viktortsolov03@gmail.com>
2021-05-02 18:33:21 +0300

    JS - Arrays

--- 03. JS-Advanced/01. JS-Advanced/02. Arrays and Nested Arrays - Lab/biggerHalf.js
+++ 03. JS-Advanced/01. JS-Advanced/02. Arrays and Nested Arrays - Lab/biggerHalf.js
@@ -1,5 +1,8 @@
 function solution(array) {
-    array.sort((a, b) => a - b);
-
-    
+    return array
+        .sort((a, b) => a - b)
+        .slice(array.length / 2);
 }
+
+console.log(solution([4, 7, 2, 5]));
+console.log(solution([3, 19, 14, 7, 2, 19, 6]));
--- 03. JS-Advanced/01. JS-Advanced/02. Arrays and Nested Arrays - Lab/lastKNumbersSequence.js
+++ 03. JS-Advanced/01. JS-Advanced/02. Arrays and Nested Arrays - Lab/lastKNumbersSequence.js
@@ -1,3 +1,3 @@
 function solution(numberOne, numberTwo){
-
+    
 }
