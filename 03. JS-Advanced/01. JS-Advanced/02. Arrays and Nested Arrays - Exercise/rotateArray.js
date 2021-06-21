3627d731493b5ba1c9781f51f65928ec1d4d7296
Viktor Tsolov <viktortsolov03@gmail.com>
2021-06-21 17:32:14 +0300

    JS - Arrays

---
+++ 03. JS-Advanced/01. JS-Advanced/02. Arrays and Nested Arrays - Exercise/rotateArray.js
@@ -0,0 +1,15 @@
+function solve(array, rotation) {
+
+    for (let i = 0; i < rotation; i++) {
+        let num = array.pop();
+        array.unshift(num);
+    }
+
+    return array.join(' ');
+}
+
+  console.log(solve(['1',  
+  '2',  
+  '3',  
+  '4'],  
+  2 ))
