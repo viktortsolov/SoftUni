7537028a65b4042f901431844e2c4fe5b3b8146c
Viktor Petrov Tsolov <viktortsolov03@gmail.com>
2021-11-30 11:14:36 +0200

    ExamPreparation

---
+++ 04. CSharp-Database/02. Entity Framework Core/ExamPreparation/C# DB Advanced Exam - 14 August 2020/SoftJail/Data/Models/Cell.cs
@@ -0,0 +1,30 @@
+﻿namespace SoftJail.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Cell
+    {
+        public Cell()
+        {
+            this.Prisoners = new HashSet<Prisoner>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
+        public int CellNumber { get; set; }
+
+        [Required]
+        public bool HasWindow1 { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public virtual ICollection<Prisoner> Prisoners { get; set; }
+    }
+}
--- 04. CSharp-Database/02. Entity Framework Core/ExamPreparation/C# DB Advanced Exam - 14 August 2020/SoftJail/Data/Models/Prisoner.cs
+++ 04. CSharp-Database/02. Entity Framework Core/ExamPreparation/C# DB Advanced Exam - 14 August 2020/SoftJail/Data/Models/Prisoner.cs
@@ -1,7 +1,45 @@
 ﻿namespace SoftJail.Data.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Prisoner
     {
+        public Prisoner()
+        {
+            this.Mails = new HashSet<Mail>();
+            this.PrisonerOfficers = new HashSet<OfficerPrisoner>();
+        }
 
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Nickname { get; set; }
+
+        [Required]
+        [Range(18, 65)]
+        public int Age { get; set; }
+
+        [Required]
+        public DateTime IncarcerationDate { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        [Range(0, (double)decimal.MaxValue)]
+        public decimal? Bail { get; set; }
+
+        [ForeignKey(nameof(Cell))]
+        public int CellId { get; set; }
+        public Cell Cell { get; set; }
+
+        public virtual ICollection<Mail> Mails { get; set; }
+        public virtual ICollection<OfficerPrisoner> PrisonerOfficers { get; set; }
     }
 }
